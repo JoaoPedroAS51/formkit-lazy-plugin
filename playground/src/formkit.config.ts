2c78bca9e3dd45af309924b0fda09d26c75b3ce9
João Pedro Antunes Silva <joao-pedroas@hotmail.com>
2023-05-14 05:28:19 -0300

    demo: init playground

---
+++ playground/src/formkit.config.ts
@@ -0,0 +1,9 @@
+import '@formkit/themes/genesis'
+import { genesisIcons } from '@formkit/icons'
+import { DefaultConfigOptions } from '@formkit/vue'
+
+const config: DefaultConfigOptions = {
+  icons: { ...genesisIcons },
+}
+
+export default config
