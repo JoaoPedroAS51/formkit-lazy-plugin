273b1a7ee94596e20d4a784b5dad5eba4057bbd4
João Pedro Antunes Silva <joao-pedroas@hotmail.com>
2023-05-14 05:18:00 -0300

    feat: createLazyPlugin

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,78 @@
-export default function test() {}
+import {
+  reset,
+  createMessage,
+  type FormKitNode,
+  type FormKitPlugin,
+} from '@formkit/core'
+import { init, undefine } from '@formkit/utils'
+
+declare module '@formkit/core' {
+  // TODO: Add `lazy` prop to FormKitProps
+  // interface FormKitProps {
+  //   lazy: (() => Promise<object>) | Promise<object>
+  // }
+
+  interface FormKitHooks {
+    beforeLazy: FormKitDispatcher<Record<string, any> | false>
+  }
+}
+
+/**
+ * The options to be passed to {@link createLazyPlugin|createLazyPlugin}
+ *
+ * @public
+ */
+export interface LazyOptions {
+  //
+}
+
+/**
+ * Creates a new lazy plugin.
+ *
+ * @param LazyOptions - The options of {@link LazyOptions|LazyOptions} to pass to the plugin
+ *
+ * @returns A {@link @formkit/core#FormKitPlugin|FormKitPlugin}
+ *
+ * @public
+ */
+export function createLazyPlugin(LazyOptions?: LazyOptions): FormKitPlugin {
+  const lazyPlugin = (node: FormKitNode) => {
+    node.addProps(['lazy'])
+
+    node.on('created', async () => {
+      if (!undefine(node.props.lazy)) {
+        return
+      }
+
+      node.config.disabled = true
+
+      try {
+        const lazy = node.props.lazy as
+          | (() => Promise<object>)
+          | Promise<object>
+        const state = await (typeof lazy === 'function' ? lazy() : lazy)
+
+        // Set `initial` state, this will be used to reset the form.
+        node.props.initial = init(state)
+
+        // Don't populate if the hook return false.
+        if (node.hook.beforeLazy.dispatch(state) !== false) {
+          reset(node, state)
+        }
+
+        node.emit('lazy', state)
+      } catch (error) {
+        node.emit('lazy:error', error)
+      } finally {
+        node.config.disabled = false
+      }
+    })
+
+    // Default error handler
+    node.on('lazy:error', ({ payload }) => {
+      console.error(payload)
+    })
+  }
+
+  return lazyPlugin
+}
